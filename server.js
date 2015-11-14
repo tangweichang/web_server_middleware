dfd9fe11de529fffa73bb16cd0afd33dd27b10d1
tangweichang <tangweichangjlu@gmail.com>
2015-11-13 22:05:00 -0500

    web_server_middleware

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+//app.get('/', function(req, res) {
+//    res.send('Hello Express!');
+//});
+
+// about us 
+app.get('/about', function(req, res) {
+    res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT,function() {
+    console.log('Express server started on port ' + PORT + '!');
+    
+});
